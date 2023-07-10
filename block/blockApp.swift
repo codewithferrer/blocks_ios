f7deacbdb7e3f4a08503c6f11ddd14855fede8d3
danielferrer <danybarco@gmail.com>
2023-07-10 07:00:29 +0200

    Initial Commit

---
+++ block/blockApp.swift
@@ -0,0 +1,17 @@
+//
+//  blockApp.swift
+//  block
+//
+//  Created by Daniel Ferrer on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct blockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
